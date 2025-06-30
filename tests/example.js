65165fb8bf01f0e989ad4319105fda1d1141e41d
PushkarSAshtekar <pushkar.ashtekar77@gmail.com>
2025-06-30 15:03:58 +0530

    Changes

--- tests/example.js
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Homepage has correct title', async ({ page }) => {
-  await page.goto('https://nextjs.org/docs/app/getting-started/installation'); // assumes local dev server
-  await expect(page).toHaveTitle("nextjs"); // or match your actual title
-});
