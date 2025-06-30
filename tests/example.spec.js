97d536caaed2fd6b723ccda8e9c3cdacceabec4f
PushkarSAshtekar <pushkar.ashtekar77@gmail.com>
2025-06-30 15:52:21 +0530

    dlt tests

--- tests/example.spec.js
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Homepage has correct title', async ({ page }) => {
- await page.goto('https://nextjs.org/docs/app/getting-started/installation');
-await expect(page).toHaveTitle("Getting Started: Installation | Next.js");
-
-});
